03a8384268280346934cfc0b91617f5ccdf40577
Laudson Euller <laudsonps2@hotmail.com>
2026-08-17 02:26:08 -0300

    feat: configuracao inicial do projeto spring boot

---
+++ src/main/java/com/gestaocontas/GestaoContasApplication.java
@@ -0,0 +1,13 @@
+package com.gestaocontas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoContasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestaoContasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gestaocontas/GestaoContasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestaocontas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoContasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
